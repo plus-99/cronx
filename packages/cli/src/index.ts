0b05fa14cbd33d3f83c64b4d34765750cf01f356
Fizer Khan <fizer@atatus.com>
2025-09-14 16:09:14 +0530

    update version to 1.0.1

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,7 +10,7 @@ const program = new Command();
 program
   .name('cronx')
   .description('CLI for managing Cronx jobs')
-  .version('1.0.0');
+  .version('1.0.1');
 
 // Add commands
 program.addCommand(listCommand);
